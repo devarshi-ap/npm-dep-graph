eb1b290eb8b6b1fc49ab8ed11f4e911b905d1d36
Dev Patel <devarshi.ap@gmail.com>
2024-11-16 13:01:07 -0500

    add lodash debounce

--- src/components/LeftPanel.vue
+++ src/components/LeftPanel.vue
@@ -1,40 +1,43 @@
 <script>
+import debounce from 'lodash.debounce';
+import axios from 'axios';
+
 export default {
     data() {
         return {
             inputValue: '',        // Bind to the text input
-            submittedValue: ''     // Stores the submitted text
+            changedValue: ''     // Stores the submitted text
         };
     },
     methods: {
-        handleSubmit() {
-        if (this.inputValue.trim()) {
-            this.submittedValue = this.inputValue; // Save the input value
-            console.log("Submitted Value:", this.inputValue); // Log the value
-            this.inputValue = ''; // Clear the input field
-        } else {
-            alert("Please enter some text!"); // Alert if input is empty
-        }
-        }
+        handleChange() {
+            this.changedValue = this.inputValue;
+            console.log("Changed Value:", this.changedValue);
+        },
+        debouncedHandleChange: debounce(function() {
+            this.handleChange();
+        }, 500)
     }
 };
 
 </script>
 
 <template>
-    <div>
-        <div>NPM Dependency Graph</div>
-        <hr/>
-        <input 
-            type="text"
-            v-model="inputValue"
-            placeholder="Npm Package Name"
-        />
-        <button @click="handleSubmit">Submit</button>
+    <div id="left-container">
+        <div id="title">NPM Dependency Graph</div>
 
-        <div v-if="submittedValue">{{ submittedValue }}</div>
+        <div>
+            <input type="text" v-model="inputValue" @input="debouncedHandleChange" placeholder="npm package (ie. express)"/>
+            <!-- <button @click="handleSubmit">Submit</button> -->
+        </div>
+
+        <div v-if="changedValue">{{ changedValue }}</div>
     </div>
 </template>
 
 
-<style></style>
+<style scoped>
+#left-container > * {
+    margin: 1rem;
+}
+</style>
